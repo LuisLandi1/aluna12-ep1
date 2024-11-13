b2402ac98a656653e792de669c358868f681a54b
LuisLandi1 <felipe.landi@aluno.ifsp.edu.br>
2024-11-12 21:25:31 -0300

    Add files via upload

---
+++ calcula.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recebe dados</title>
+    <script>
+function envia() {
+    var capital = document.getElementById('capital').value;
+    var taxa = document.getElementById('taxa').value;
+    var tempo = document.getElementById('tempo').value;
+
+    // Construindo a URL com os parâmetros
+    var url = 'CalaculaSim.php?capital=' + capital + '&taxa=' + taxa + '&tempo=' + tempo;
+    fetch(url)
+	.then(response => response.json())
+	.then(data => {
+	    document.getElementById('mostrar').textContent = "Os juros são: R$" + data.juros.toFixed(2);
+	});
+}
+    </script>
+</head>
+<body>
+    <h1>Calculadora de Juros Simples</h1>
+<div id='mostrar'>
+	<label for="capital">Capital Inicial:</label>
+	<input type="number" id="capital">
+
+	<label for="taxa">Taxa de Juros (% a.a.):</label>
+	<input type="number" id="taxa">
+
+	<label for="tempo">Tempo (anos):</label>
+	<input type="number" id="tempo">
+
+	<input type="button" value='Calcular' onclick='envia()'>
+</div>
+
+<img src="https://media.baamboozle.com/uploads/images/120763/1656273165_685451_gif-url.gif" alt="">
+</body>
+</html>  
